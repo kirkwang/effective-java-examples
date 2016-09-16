236d638bb5084f766ec2e2035b719df4e0b7d820
kirkwang <kirkwangke@gmail.com>
2016-09-16 15:37:31 -0700

    Item 3 enum singleton

--- src/main/java/org/effectivejava/examples/chapter02/item03/enumoration/Elvis.java
+++ src/main/java/org/effectivejava/examples/chapter02/item03/enumoration/Elvis.java
@@ -2,15 +2,22 @@
 package org.effectivejava.examples.chapter02.item03.enumoration;
 
 public enum Elvis {
-	INSTANCE;
+    INSTINCE;
+
+    {
+        System.out.println("single elvis");
+    }
+
+    // This code would normally appear outside the class!
+    public static void main(String[] args) {
+        Elvis elvis = Elvis.INSTINCE;
+        Elvis eis = Elvis.INSTINCE;
+        System.out.println("Whoa baby, my name is " + eis.name());
+
+        //elvis.leaveTheBuilding();
+    }
 
 	public void leaveTheBuilding() {
 		System.out.println("Whoa baby, I'm outta here!");
-	}
-
-	// This code would normally appear outside the class!
-	public static void main(String[] args) {
-		Elvis elvis = Elvis.INSTANCE;
-		elvis.leaveTheBuilding();
 	}
 }

163939fffab68f6c628023f7288b5e64ac8aea08
kirkwang <kirkwangke@gmail.com>
2017-05-10 11:23:56 -0700

    Elivis and AppTest

--- src/main/java/org/effectivejava/examples/chapter02/item03/enumoration/Elvis.java
+++ src/main/java/org/effectivejava/examples/chapter02/item03/enumoration/Elvis.java
@@ -2,22 +2,15 @@
 package org.effectivejava.examples.chapter02.item03.enumoration;
 
 public enum Elvis {
-    INSTINCE;
-
-    {
-        System.out.println("single elvis");
-    }
-
-    // This code would normally appear outside the class!
-    public static void main(String[] args) {
-        Elvis elvis = Elvis.INSTINCE;
-        Elvis eis = Elvis.INSTINCE;
-        System.out.println("Whoa baby, my name is " + eis.name());
-
-        //elvis.leaveTheBuilding();
-    }
+	INSTANCE;
 
 	public void leaveTheBuilding() {
 		System.out.println("Whoa baby, I'm outta here!");
+	}
+
+	// This code would normally appear outside the class!
+	public static void main(String[] args) {
+		Elvis elvis = Elvis.INSTANCE;
+		elvis.leaveTheBuilding();
 	}
 }
